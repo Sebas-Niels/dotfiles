a5e942918423a0131f3806df92e6aabe45013e9e
Sebas-Niels <tryspamming@gmail.com>
2025-12-29 20:27:04 +0100

    Refactor added proper wrapping

--- modules/nixos/desktop/plasma.nix
+++ modules/nixos/desktop/plasma.nix
@@ -1,13 +1,19 @@
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+{ ... }:
+{
+  # Enable X11
+  services.xserver = {
+    enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "dk";
-    variant = "";
+    xkb = {
+      layout = "dk";
+      variant = "";
+    };
   };
+
+  # Display manager
+  services.displayManager.sddm.enable = true;
+
+  # Desktop environment
+  services.desktopManager.plasma6.enable = true;
+}
+
