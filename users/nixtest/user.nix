6bae71ffcde567c6f1436811f7456fb1383ff226
Sebas-Niels <sebas.nn@tuta.com>
2025-12-29 21:38:13 +0100

    Add user for migration

---
+++ users/nixtest/user.nix
@@ -0,0 +1,12 @@
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+{ pkgs, ... }:
+{
+  users.users.nixtest = {
+    isNormalUser = true;
+    description = "nixtest";
+    extraGroups = [ "wheel" "networkmanager" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
+  };
+}
