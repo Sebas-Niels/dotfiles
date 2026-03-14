73658bc44776e8c4f6d6d038c651383aa0a6f175
Sebastian Nielsen <sebas.nn@tuta.com>
2026-03-14 23:06:19 +0100

    refactor: modified hyprsunset.nix settings

--- modules/home-manager/wayland/hyprland/hyprsunset.nix
+++ modules/home-manager/wayland/hyprland/hyprsunset.nix
@@ -3,10 +3,18 @@
   services.hyprsunset = {
     enable = true;
 
-    profile = {
-      time = "20:00";
-      temperature = "5500";
-      gamma = "0.8";
+    settings = {
+      profile = [
+        {
+          time = "7:00";
+          identity = true;
+        }
+        {
+          time = "20:00";
+          temperature = "4000";
+          gamma = "0.90";
+        }
+      ];
     };
   };
 }
