0bf92c61af2df022b582ca4a0c485586d3d23959
Sebastian Nielsen <sebas.nn@tuta.com>
2026-05-15 17:56:57 +0200

    feat: wooting keyboard device recognition

--- hosts/lambda/default.nix
+++ hosts/lambda/default.nix
@@ -64,6 +64,22 @@ programs.appimage = {
     nixrb = "sudo nixos-rebuild switch --flake .#$(hostname)";
   };
 
+  services.udev.extraRules = ''
+  # Wooting One Legacy
+  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff01", TAG+="uaccess"
+  SUBSYSTEM=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff01", TAG+="uaccess"
+  # Wooting One update mode
+  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2402", TAG+="uaccess"
+  # Wooting Two Legacy
+  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff02", TAG+="uaccess"
+  SUBSYSTEM=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff02", TAG+="uaccess"
+  # Wooting Two update mode
+  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2403", TAG+="uaccess"
+  # Generic Wooting devices
+  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="31e3", TAG+="uaccess"
+  SUBSYSTEM=="usb", ATTRS{idVendor}=="31e3", TAG+="uaccess"
+'';
+
   nix.settings.download-buffer-size = 524288000; # 500MB
 
   system.stateVersion = "25.11";
