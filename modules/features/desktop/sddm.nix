f121d0fea1fa7477e59d0412c5fbfe8f9475c79b
Sebastian Nielsen <sebas.nn@tuta.com>
2026-08-23 17:34:21 +0200

    feature: implement sddm theme

--- modules/features/desktop/sddm.nix
+++ modules/features/desktop/sddm.nix
@@ -1,6 +1,20 @@
 { ... }: {
-    flake.nixosModules.sddm = { ... }: {
-      services.xserver.enable = true;
-      services.displayManager.sddm.enable = true;
+  flake.nixosModules.sddm = { pkgs, ... }: let
+    theme = pkgs.where-is-my-sddm-theme.override {
+      themeConfig.General = {
+        passwordCharacter = "*";
+        passwordFontSize = 96;
+        backgroundFill = "#000000";
+        basicTextColor = "#ffffff";
+      };
     };
+  in {
+    services.xserver.enable = true;
+
+    services.displayManager.sddm = {
+      enable = true;
+      theme = "${theme}/share/sddm/themes/where_is_my_sddm_theme";
+      extraPackages = [ theme ];
+    };
+  };
 }
