efd5fe4a2c582ef2894a108fc8415ca32cc69138
Sebastian Nielsen <sebas.nn@tuta.com>
2026-08-23 19:18:16 +0200

    feature: implement audio cutoff fix

--- modules/features/system/audio.nix
+++ modules/features/system/audio.nix
@@ -1,13 +1,47 @@
 { ... }: {
-    flake.nixosModules.pipewireAudio = { ... }: {
-      services.pulseaudio.enable = false;
-      security.rtkit.enable = true;
+  flake.nixosModules.pipewireAudio = { pkgs, ... }: {
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
 
-      services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-      };
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+
+      wireplumber.configPackages = [
+        (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/51-disable-suspend.conf" ''
+          monitor.alsa.rules = [
+            {
+              matches = [
+                {
+                  device.name = "~alsa_card.*"
+                }
+              ]
+              actions = {
+                update-props = {
+                  api.alsa.use-acp = true
+                }
+              }
+            }
+            {
+              matches = [
+                {
+                  node.name = "~alsa_input.*"
+                }
+                {
+                  node.name = "~alsa_output.*"
+                }
+              ]
+              actions = {
+                update-props = {
+                  session.suspend-timeout-seconds = 0
+                }
+              }
+            }
+          ]
+        '')
+      ];
     };
+  };
 }
