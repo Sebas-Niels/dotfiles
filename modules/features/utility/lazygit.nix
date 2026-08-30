5ba339038fbd3a274c3f5293e4dc13532bba95b2
Sebastian Nielsen <sebas.nn@tuta.com>
2026-08-30 10:52:35 +0200

    refactor: change lazygit to remove misc. status info, such as copyright, tutorial, etc

--- modules/features/utility/lazygit.nix
+++ modules/features/utility/lazygit.nix
@@ -13,10 +13,14 @@
         configFile = (pkgs.formats.yaml { }).generate "lazygit-config.yml" {
           gui.mouseEvents = false;
         };
+
+        lazygit' = pkgs.lazygit.overrideAttrs (old: {
+          patches = (old.patches or [ ]) ++ [ ./lazygit-bare-dashboard.patch ];
+        });
       in
       pkgs.symlinkJoin {
         name = "lazygit";
-        paths = [ pkgs.lazygit ];
+        paths = [ lazygit' ];
         nativeBuildInputs = [ pkgs.makeWrapper ];
         postBuild = ''
           wrapProgram $out/bin/lazygit --set LG_CONFIG_FILE ${configFile}
