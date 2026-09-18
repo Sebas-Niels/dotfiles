471442c15b976cc39946460c153beadbae340d6c
Sebastian Nielsen <sebas.nn@tuta.com>
2026-09-18 22:54:28 +0200

    fix: removed comments and changed to pure json

--- modules/features/utility/fastfetch/fastfetch.nix
+++ modules/features/utility/fastfetch/fastfetch.nix
@@ -13,7 +13,7 @@
     {
       packages.lambdaFastfetch = inputs.wrapper-modules.wrappers.fastfetch.wrap {
         inherit pkgs;
-        settings = builtins.fromJSON (builtins.readFile ./config.jsonc);
+        settings = builtins.fromJSON (builtins.readFile ./config.json);
       };
     };
 }
